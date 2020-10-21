fa783ded38b5c24235e8cbf7e049a939bf56b78a
Leon <kliwaru@gmail.com>
2020-10-21 17:18:54 +0800

    main

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -15,7 +15,7 @@ class WelcomesController < ApplicationController
         @approval_url = "https://eghl-test.herokuapp.com/payment_response_success_redirect/#{@payment.id}"
         @unapproval_url = "https://eghl-test.herokuapp.com/payment_response_fail_redirect/#{@payment.id}"
 
-        @payment_id = "TESTHOST#{Time.now.strftime("%d%m%Y%H%M")}"
+        @payment_id = "TEST#{Time.now.strftime('%d%m%Y%H%M%S')}"
         @hashval = Digest::SHA2.hexdigest("#{@api_pass}#{@api_id}#{@payment_id}#{@return_url}#{@approval_url}#{@unapproval_url}#{@callback_url}#{@amount}MYR192.168.2.35780")
     end
 
@@ -28,7 +28,7 @@ class WelcomesController < ApplicationController
         @return_url = "https://eghl-test.herokuapp.com/await_payment_response_redirect/#{@payment.id}"
         @approval_url = "https://eghl-test.herokuapp.com/payment_response_success_redirect/#{@payment.id}"
         @unapproval_url = "https://eghl-test.herokuapp.com/payment_response_fail_redirect/#{@payment.id}"
-        @payment_id = "TESTHOST#{Time.now.strftime("%d%m%Y%H%M")}"
+        @payment_id = "TEST#{Time.now.strftime('%d%m%Y%H%M%S')}"
         
         req = Faraday.new do |f|
             f.adapter :net_http
@@ -44,7 +44,7 @@ class WelcomesController < ApplicationController
                 "PymtMethod": "ANY",
                 "ServiceID": @api_id, # for
                 "PaymentID": @payment_id,
-                "OrderNumber": "IJKLMN",
+                "OrderNumber": @payment_id,
                 "PaymentDesc": "-",
                 "MerchantApprovalURL": @approval_url, # redirect path when payment successful
                 "MerchantUnApprovalURL": @unapproval_url, # redirect path when payment fail
